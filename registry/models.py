f6d2fd313608b3ba3f47ce886f18ba23bd3b7444
Hrishikesh Ballal <hballal@gmail.com>
2019-04-08 08:25:22 +0100

    Updated models for country codes and certifications

--- registry/models.py
+++ registry/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+    from django.db import models
 import uuid
 # Create your models here.
 from datetime import date
@@ -48,7 +48,8 @@ class Authorization(models.Model):
         return self.title
 
 class Operator(models.Model):
-    OPTYPE_CHOICES = ((0, _('NA')),(1, _('LUC')),(2, _('Non-LUC')),)
+    COUNTRY_CHOICES_ISO3166=(('AF','AFGHANISTAN'),('AX','ÅLAND ISLANDS'),('AL','ALBANIA'),('DZ','ALGERIA'),('AS','AMERICAN SAMOA'),('AD','ANDORRA'),('AO','ANGOLA'),('AI','ANGUILLA'),('AQ','ANTARCTICA'),('AG','ANTIGUA AND BARBUDA'),('AR','ARGENTINA'),('AM','ARMENIA'),('AW','ARUBA'),('AU','AUSTRALIA'),('AT','AUSTRIA'),('AZ','AZERBAIJAN'),('BS','BAHAMAS'),('BH','BAHRAIN'),('BD','BANGLADESH'),('BB','BARBADOS'),('BY','BELARUS'),('BE','BELGIUM'),('BZ','BELIZE'),('BJ','BENIN'),('BM','BERMUDA'),('BT','BHUTAN'),('BO','BOLIVIA, PLURINATIONAL STATE OF'),('BQ','BONAIRE, SINT EUSTATIUS AND SABA'),('BA','BOSNIA AND HERZEGOVINA'),('BW','BOTSWANA'),('BV','BOUVET ISLAND'),('BR','BRAZIL'),('IO','BRITISH INDIAN OCEAN TERRITORY'),('BN','BRUNEI DARUSSALAM'),('BG','BULGARIA'),('BF','BURKINA FASO'),('BI','BURUNDI'),('KH','CAMBODIA'),('CM','CAMEROON'),('CA','CANADA'),('CV','CAPE VERDE'),('KY','CAYMAN ISLANDS'),('CF','CENTRAL AFRICAN REPUBLIC'),('TD','CHAD'),('CL','CHILE'),('CN','CHINA'),('CX','CHRISTMAS ISLAND'),('CC','COCOS (KEELING) ISLANDS'),('CO','COLOMBIA'),('KM','COMOROS'),('CG','CONGO'),('CD','CONGO, THE DEMOCRATIC REPUBLIC OF THE'),('CK','COOK ISLANDS'),('CR','COSTA RICA'),('CI','CÔTE D\'IVOIRE'),('HR','CROATIA'),('CU','CUBA'),('CW','CURAÇAO'),('CY','CYPRUS'),('CZ','CZECH REPUBLIC'),('DK','DENMARK'),('DJ','DJIBOUTI'),('DM','DOMINICA'),('DO','DOMINICAN REPUBLIC'),('EC','ECUADOR'),('EG','EGYPT'),('SV','EL SALVADOR'),('GQ','EQUATORIAL GUINEA'),('ER','ERITREA'),('EE','ESTONIA'),('ET','ETHIOPIA'),('FK','FALKLAND ISLANDS (MALVINAS)'),('FO','FAROE ISLANDS'),('FJ','FIJI'),('FI','FINLAND'),('FR','FRANCE'),('GF','FRENCH GUIANA'),('PF','FRENCH POLYNESIA'),('TF','FRENCH SOUTHERN TERRITORIES'),('GA','GABON'),('GM','GAMBIA'),('GE','GEORGIA'),('DE','GERMANY'),('GH','GHANA'),('GI','GIBRALTAR'),('GR','GREECE'),('GL','GREENLAND'),('GD','GRENADA'),('GP','GUADELOUPE'),('GU','GUAM'),('GT','GUATEMALA'),('GG','GUERNSEY'),('GN','GUINEA'),('GW','GUINEA-BISSAU'),('GY','GUYANA'),('HT','HAITI'),('HM','HEARD ISLAND AND MCDONALD ISLANDS'),('VA','HOLY SEE (VATICAN CITY STATE)'),('HN','HONDURAS'),('HK','HONG KONG'),('HU','HUNGARY'),('IS','ICELAND'),('IN','INDIA'),('ID','INDONESIA'),('IR','IRAN, ISLAMIC REPUBLIC OF'),('IQ','IRAQ'),('IE','IRELAND'),('IM','ISLE OF MAN'),('IL','ISRAEL'),('IT','ITALY'),('JM','JAMAICA'),('JP','JAPAN'),('JE','JERSEY'),('JO','JORDAN'),('KZ','KAZAKHSTAN'),('KE','KENYA'),('KI','KIRIBATI'),('KP','KOREA, DEMOCRATIC PEOPLE\'S REPUBLIC OF'),('KR','KOREA, REPUBLIC OF'),('KW','KUWAIT'),('KG','KYRGYZSTAN'),('LA','LAO PEOPLE\'S DEMOCRATIC REPUBLIC'),('LV','LATVIA'),('LB','LEBANON'),('LS','LESOTHO'),('LR','LIBERIA'),('LY','LIBYAN ARAB JAMAHIRIYA'),('LI','LIECHTENSTEIN'),('LT','LITHUANIA'),('LU','LUXEMBOURG'),('MO','MACAO'),('MK','MACEDONIA, THE FORMER YUGOSLAV REPUBLIC OF'),('MG','MADAGASCAR'),('MW','MALAWI'),('MY','MALAYSIA'),('MV','MALDIVES'),('ML','MALI'),('MT','MALTA'),('MH','MARSHALL ISLANDS'),('MQ','MARTINIQUE'),('MR','MAURITANIA'),('MU','MAURITIUS'),('YT','MAYOTTE'),('MX','MEXICO'),('FM','MICRONESIA, FEDERATED STATES OF'),('MD','MOLDOVA, REPUBLIC OF'),('MC','MONACO'),('MN','MONGOLIA'),('ME','MONTENEGRO'),('MS','MONTSERRAT'),('MA','MOROCCO'),('MZ','MOZAMBIQUE'),('MM','MYANMAR'),('NA','NAMIBIA'),('NR','NAURU'),('NP','NEPAL'),('NL','NETHERLANDS'),('NC','NEW CALEDONIA'),('NZ','NEW ZEALAND'),('NI','NICARAGUA'),('NE','NIGER'),('NG','NIGERIA'),('NU','NIUE'),('NF','NORFOLK ISLAND'),('MP','NORTHERN MARIANA ISLANDS'),('NO','NORWAY'),('OM','OMAN'),('PK','PAKISTAN'),('PW','PALAU'),('PS','PALESTINIAN TERRITORY, OCCUPIED'),('PA','PANAMA'),('PG','PAPUA NEW GUINEA'),('PY','PARAGUAY'),('PE','PERU'),('PH','PHILIPPINES'),('PN','PITCAIRN'),('PL','POLAND'),('PT','PORTUGAL'),('PR','PUERTO RICO'),('QA','QATAR'),('RE','RÉUNION'),('RO','ROMANIA'),('RU','RUSSIAN FEDERATION'),('RW','RWANDA'),('BL','SAINT BARTHÉLEMY'),('SH','SAINT HELENA, ASCENSION AND TRISTAN DA CUNHA'),('KN','SAINT KITTS AND NEVIS'),('LC','SAINT LUCIA'),('MF','SAINT MARTIN (FRENCH PART)'),('PM','SAINT PIERRE AND MIQUELON'),('VC','SAINT VINCENT AND THE GRENADINES'),('WS','SAMOA'),('SM','SAN MARINO'),('ST','SAO TOME AND PRINCIPE'),('SA','SAUDI ARABIA'),('SN','SENEGAL'),('RS','SERBIA'),('SC','SEYCHELLES'),('SL','SIERRA LEONE'),('SG','SINGAPORE'),('SX','SINT MAARTEN (DUTCH PART)'),('SK','SLOVAKIA'),('SI','SLOVENIA'),('SB','SOLOMON ISLANDS'),('SO','SOMALIA'),('ZA','SOUTH AFRICA'),('GS','SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS'),('SS','SOUTH SUDAN'),('ES','SPAIN'),('LK','SRI LANKA'),('SD','SUDAN'),('SR','SURINAME'),('SJ','SVALBARD AND JAN MAYEN'),('SZ','SWAZILAND'),('SE','SWEDEN'),('CH','SWITZERLAND'),('SY','SYRIAN ARAB REPUBLIC'),('TW','TAIWAN, PROVINCE OF CHINA'),('TJ','TAJIKISTAN'),('TZ','TANZANIA, UNITED REPUBLIC OF'),('TH','THAILAND'),('TL','TIMOR-LESTE'),('TG','TOGO'),('TK','TOKELAU'),('TO','TONGA'),('TT','TRINIDAD AND TOBAGO'),('TN','TUNISIA'),('TR','TURKEY'),('TM','TURKMENISTAN'),('TC','TURKS AND CAICOS ISLANDS'),('TV','TUVALU'),('UG','UGANDA'),('UA','UKRAINE'),('AE','UNITED ARAB EMIRATES'),('GB','UNITED KINGDOM'),('US','UNITED STATES'),('UM','UNITED STATES MINOR OUTLYING ISLANDS'),('UY','URUGUAY'),('UZ','UZBEKISTAN'),('VU','VANUATU'),('VE','VENEZUELA, BOLIVARIAN REPUBLIC OF'),('VN','VIET NAM'),('VG','VIRGIN ISLANDS, BRITISH'),('VI','VIRGIN ISLANDS, U.S.'),('WF','WALLIS AND FUTUNA'),('EH','WESTERN SAHARA'),('YE','YEMEN'),('ZM','ZAMBIA'),('ZW','ZIMBABWE'),)    
+    OPTYPE_CHOICES = ((0, _('NA')),(1, _('LUC')),(2, _('Non-LUC')),(3, _('AUTH')),(4, _('DEC')),)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     company_name = models.CharField(max_length=280)
     website = models.URLField()
@@ -66,6 +67,7 @@ class Operator(models.Model):
     vat_number = models.CharField(max_length=25, blank=True, null=True)
     insurance_number = models.CharField(max_length=25, blank=True, null=True)
     company_number = models.CharField(max_length=25, blank=True, null=True)
+    operator_type = models.CharField(max_length = 2, choices=COUNTRY_CHOICES_ISO3166, default = 'NA')
 
     def __unicode__(self):
        return self.company_name
@@ -99,9 +101,11 @@ class Contact(models.Model):
         return self.email
 
 class RpasTest(models.Model):
-    TESTTYPE_CHOICES = ((0, _('Online Test')),(1, _('In Authorized Test Center')),(2, _('Other')),)
+    TESTTYPE_CHOICES = ((0, _('Remote pilot online theoretical competency')),(1, _('Certificate of remote pilot competency')),(2, _('Other')),)
+    TAKEN_AT_CHOICES = ((0, _('Online Test')),(1, _('In Authorized Test Center')),(2, _('Other')),)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     test_type = models.IntegerField(choices = TESTTYPE_CHOICES, default =0)
+    taken_at = models.IntegerField(choices = TAKEN_AT_CHOICES, default =0)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
